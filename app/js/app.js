fe761a7e2aa6b4972c45129700369fed7467f604
Martin Hotell <hochelmartin@gmail.com>
2013-08-27 23:45:31 +0200

    angular functionality, beta done 0.9b

--- app/js/app.js
+++ app/js/app.js
@@ -3,11 +3,26 @@
 
 // Declare app level module which depends on filters, and services
 angular.module('myApp', ['ngRoute', 'myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers'])
-    .config(['$routeProvider', function ($routeProvider) {
+    .config(['$routeProvider','$locationProvider', function ($routeProvider, $locationProvider) {
+
+        $locationProvider.hashPrefix('!');
+
         $routeProvider
-            .when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
+            .when('/vydavatelia', {
+                templateUrl: 'partials/vydavatelia.html',
+                controller: 'PublishersCtrl'
+            });
         $routeProvider
-            .when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+            .when('/kontakt', {
+                templateUrl: 'partials/kontakt.html',
+                controller: 'ContactCtrl'
+            });
         $routeProvider
-            .otherwise({redirectTo: '/view1'});
+            .otherwise({
+                templateUrl: 'partials/home.html',
+                controller: 'HomeCtrl',
+                redirectTo: ''
+            });
     }]);
+
+angular.module('myApp').run()
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,74 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-    .controller('MyCtrl1', [function() {
+    .controller('MainCtrl', [
+        '$scope', '$location',
+        function($scope, $location) {
 
-  }])
-  .controller('MyCtrl2', [function() {
+            $scope.menuActive = function(url, exactMatch) {
+                if (exactMatch) {
+                    return $location.path() === url;
+                }
+                else {
+                    return $location.path().indexOf(url) == 0;
+                }
 
-  }]);
+            }
+
+            $scope.isRouteActive = function(page) {
+//                var regExp = new RegExp("^" + url + "$");
+//                console.log($location.path());
+//                console.log($location.path().match(regExp));
+//                return $location.path().match(regExp);
+//                console.log($location.path().substring(1));
+                return page === $location.path().substring(1);
+            }
+
+        }
+    ])
+    .controller('HomeCtrl', [ '$scope', '$location',
+        function($scope, $location) {
+
+            $scope.go = function ( path ) {
+                $location.path( path );
+            };
+
+        }
+    ])
+    .controller('PublishersCtrl', [ '$scope',
+        function($scope) {
+
+            $scope.publishersList = [
+                {
+                    name: 'IGI Global',
+                    url: 'http://igi-global.com'
+                },
+                {
+                    name: 'Cengage',
+                    url: 'http://www.cengage.com/'
+                },
+                {
+                    name: 'Cambridge University Press',
+                    url: 'http://cambridge.com'
+                },
+                {
+                    name: 'Oxford University Press',
+                    url: 'http://ukcatalogue.oup.com/'
+                },
+                {
+                    name: 'Springer',
+                    url: 'http://www.springer.com/'
+                },
+                {
+                    name: 'Pearson Education',
+                    url: 'http://www.pearson.com/'
+                }
+            ]
+
+        }
+    ])
+    .controller('ContactCtrl', [
+        function() {
+
+        }
+    ]);
